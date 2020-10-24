90ecfad0acfcb4e09ae278458ba7bcded7a5d561
Alex Sderjikov <sderjikov.al@gmail.com>
2020-10-25 01:06:28 +0300

    24.10.2020 v10

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
 
 
 app = Flask(__name__)
@@ -6,5 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('index.html')
+
+
+@app.route('/share_valut')
+def share_valut():
+    return render_template('share_valut')
+
+
+@app.route('/history')
+def history():
+    return render_template('history')
 
 
